365691658bcdeb8e3121c4f5e32fca26ec7c29cd
Chris H <crishack8@gmail.com>
2021-02-17 13:16:29 -0600

    Fix icons

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex justify-center items-center md:block">
     <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="home" type="fas" class="m-2 w-6 h-6"></fa>
+      <fa icon="home" type="fas" class="m-2 w-10 h-10"></fa>
       <p>Bondurant IA 50035</p>
     </div>
     <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="phone" type="fas" class="m-2 w-6 h-6"></fa>
+      <fa icon="phone" type="fas" class="m-2 w-10 h-10"></fa>
       <p>
         <a class="underline" href="tel:1-515-314-8915">515-314-8915</a>
       </p>
@@ -13,13 +13,13 @@
   </div>
   <div class="flex justify-center items-center md:block">
     <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="envelope" type="fas" class="m-2 w-6 h-6"></fa>
+      <fa icon="envelope" type="fas" class="m-2 w-10 h-10"></fa>
       <p>
         <a class="underline" href="mailto:crishack8@gmail.com">crishack8@gmail.com</a>
       </p>
     </div>
     <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="code" type="fas" class="m-2 w-6 h-6"></fa>
+      <fa icon="code" type="fas" class="m-2 w-10 h-10"></fa>
       <p>
         <a class="underline" href="https://github.com/chx2">github.com/chx2</a>
       </p>
