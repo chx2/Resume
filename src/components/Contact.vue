eaee6a1342b57d1253bac3235a76e6c289d4a15f
Chris H <crishack8@gmail.com>
2021-02-24 16:05:16 -0600

    Update print contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="flex justify-center items-center md:block">
-    <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="home" type="fas" class="m-2 w-10 h-10"></fa>
+  <div class="flex justify-center items-center print:block md:block">
+    <div class="w-1/2 print:w-full md:w-full flex justify-start items-center print:text-black">
+      <fa icon="home" type="fas" class="print:hidden m-2 w-10 h-10"></fa>
       <p>Bondurant IA 50035</p>
     </div>
-    <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="phone" type="fas" class="m-2 w-10 h-10"></fa>
+    <div class="w-1/2 print:w-full md:w-full flex justify-start items-center print:text-black">
+      <fa icon="phone" type="fas" class="print:hidden m-2 w-10 h-10"></fa>
       <p>
         <a class="underline" href="tel:1-515-314-8915">515-314-8915</a>
       </p>
     </div>
   </div>
-  <div class="flex justify-center items-center md:block">
-    <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="envelope" type="fas" class="m-2 w-10 h-10"></fa>
+  <div class="flex justify-center items-center print:block md:block">
+    <div class="w-1/2 print:w-full md:w-full flex justify-start items-center print:text-black">
+      <fa icon="envelope" type="fas" class="print:hidden m-2 w-10 h-10"></fa>
       <p>
         <a class="underline" href="mailto:crishack8@gmail.com">crishack8@gmail.com</a>
       </p>
     </div>
-    <div class="w-1/2 md:w-full flex justify-start items-center print:text-black">
-      <fa icon="code" type="fas" class="m-2 w-10 h-10"></fa>
+    <div class="w-1/2 print:hidden md:w-full flex justify-start items-center print:text-black">
+      <fa icon="code" type="fas" class="print:hidden m-2 w-10 h-10"></fa>
       <p>
         <a class="underline" href="https://github.com/chx2">github.com/chx2</a>
       </p>
